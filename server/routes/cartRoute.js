2e0d30345ae2b0f8f1b1fc04ee778bf6bbbf0f05
R vamsi krishna <vamsiprince08@gmail.com>
2026-06-26 13:37:04 +0530

    fix

--- server/routes/cartRoute.js
+++ server/routes/cartRoute.js
@@ -1,6 +1,6 @@
 import express from "express"; //  Fix: Import express instead of mongoose
 import authUser from "../middlewares/authUser.js";
-import { updateCart } from "../controllers/cartController.js";
+import { updateCart } from "../controllers/cartcontroller.js";
 
 //  Fix: Use express.Router() so the router initializes correctly
 const cartRouter = express.Router();
